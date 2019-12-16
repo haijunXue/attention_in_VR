7aafa6dd502f4d40930a235b010e60acdff25161
AmrSaadi <amr_saadi@hotmail.com>
2019-12-16 18:40:32 +0100

    setup steamVR

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // retrieves the actual control information
+    public SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+        }
+    }
+    
+   
+}
