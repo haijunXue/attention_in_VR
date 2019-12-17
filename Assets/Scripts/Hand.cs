0ec7620e20bba95c5d41f80aab687ca5fe045814
haijunXue <xuehaijun793g@gmail.com>
2019-12-17 18:03:41 +0100

    add vr controller

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_ContactInteractables = new List<Interactable>();
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Down
+        if (m_GrabAction.GetStateDown(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Down");
+            PickUp();
+        }
+
+        // Up
+        if (m_GrabAction.GetStateUp(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Up");
+            Drop();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+        m_ContactInteractables.Add(other.gameObject.GetComponent<Interactable>());
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+        m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+    }
+
+    public void PickUp()
+    {
+
+    }
+
+    public void Drop()
+    {
+
+    }
+
+    public Interactable GetNearestInteractable()
+    {
+        return null;
+    }
+}
