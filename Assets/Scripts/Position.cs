1343723dc1ff7642a462d1313e72cc2774e1b97d
haijunXue <xuehaijun793g@gmail.com>
2020-07-08 23:11:16 +0200

    four tasks

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    
+    public Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.tag);
+        pos = transform.position;
+        Debug.Log(" pos is " + pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
