2f4854cbdf6394c37652469bba0201df7220d492
haijunXue <xuehaijun793g@gmail.com>
2020-07-27 19:30:50 +0200

    last version

---
+++ Assets/Scripts/screenshotHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenshotHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static screenshotHandler instance;
+    private Camera myCamera;
+    private bool takeScreenshotOnNextFrame;
+
+    private void Awake()
+    {
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+        if(takeScreenshotOnNextFrame){
+            takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0,0,renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0,0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot.png", byteArray);
+            Debug.Log("Save the camerascreenshot.png");
+   
+
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            myCamera.targetTexture = null;
+        }
+    }
+
+    private void TakeScreenshot(int width, int height)
+    {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextFrame = true;
+    }
+
+    public static void TakeScreenshot_Static(int width, int height)
+    {
+        instance.TakeScreenshot(width, height);
+    }
+}
